c75e2e597266e8d14b93362d4e5c14ff36ab8e4a
Anthony Mirand <anthonypmirand@gmail.com>
2016-11-23 02:28:59 -0800

    Include sample configuration file

---
+++ spotted_on_spotify/conf_cleaned.py
@@ -0,0 +1,10 @@
+# A configuration file for Spotted on Spotify
+# Included for structure and readability
+
+VERSION="0.1.5"
+
+SPOTIPY_CLIENT_ID=""
+SPOTIPY_CLIENT_SECRET=""
+SPOTIPY_REDIRECT_URI="https://github.com/anthonymirand/SpottedOnSpotify-cmdline"
+
+ACOUSTID_API_KEY=""
