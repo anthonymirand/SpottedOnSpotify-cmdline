aa39ac5c08627700607b6d232112eb2cfe644233
Anthony Mirand <anthonypmirand@gmail.com>
2016-11-23 02:23:20 -0800

    Hide configuration file

--- spotted_on_spotify/conf.py
+++
@@ -1,9 +0,0 @@
-# A configuration file for Spotted on Spotify
-
-VERSION="0.1.5"
-
-SPOTIPY_CLIENT_ID="a6a7974f555849a8b63e9191ebc12629"
-SPOTIPY_CLIENT_SECRET="c13b81df0c33434ea4596c9589ca1aab"
-SPOTIPY_REDIRECT_URI="https://github.com/anthonymirand/SpottedOnSpotify-cmdline"
-
-ACOUSTID_API_KEY="qY3621bc1V"
